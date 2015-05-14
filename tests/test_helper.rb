ffea9358589844fd050931b4471b59b23639fed5
Bryan Finlayson <bdfinlayson@gmail.com>
2015-05-13 22:36:35 -0500

    refactored test_helper and executable file for new file structure

--- tests/test_helper.rb
+++ tests/test_helper.rb
@@ -2,11 +2,25 @@ require 'rubygems'
 require 'bundler/setup'
 require 'minitest/reporters'
 require 'minitest/autorun'
+Dir["./app/**/*.rb"].each { |file| require file }
+Dir["./lib/*.rb"].each { |file| require file }
+ENV["TEST"] = "true"
 
 reporter_options = { color: true }
 
 Minitest::Reporters.use!
 [Minitest::Reporters::DefaultReporter.new(reporter_options)]
+
+class Minitest::Test
+def setup
+  Database.load_structure
+  Database.execute("delete from rooms")
+end
+end
+
+def create_room(room,description)
+  Database.execute("insert into rooms (room,description) values (?,?)", room, description)
+end
 
 def main_menu
   "1. Add room\n2. Quit\n"
