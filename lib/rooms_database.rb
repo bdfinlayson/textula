95fb73299021ebf87c7b38cebbd95bab503a74b6
Bryan Finlayson <bdfinlayson@gmail.com>
2015-05-13 22:42:47 -0500

    add lib directory. Remove data directory and files

---
+++ lib/rooms_database.rb
@@ -0,0 +1,22 @@
+require 'sqlite3'
+
+class RoomsDatabase
+
+  def self.load_structure
+    RoomsDatabase.execute("create table if not exists rooms(id integer primary key autoincrement, room text, description text, next_to text)")
+  end
+
+  def self.execute(*args)
+    initialize_database unless defined?(@@db)
+    @@db.execute(*args)
+  end
+
+  def self.initialize_database
+    directory = "db"
+    Dir.mkdir(directory) unless File.exist?(directory)
+    environment = ENV["TEST"] ? "test" : "production"
+    database = "#{directory}/rooms_database_#{environment}.sqlite"
+    @@db = SQLite3::Database.new( database  )
+  end
+
+end
