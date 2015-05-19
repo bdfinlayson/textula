aeb610351dee5eb28fb4e7be018f6373bf644ff1
Bryan Finlayson <bdfinlayson@gmail.com>
2015-05-19 10:46:40 -0500

    add methods to help find the player's current and new locations

--- app/models/exits_model.rb
+++ app/models/exits_model.rb
@@ -10,4 +10,12 @@ class ExitsModel
     Database.execute("insert into exits (child_room_id,parent_room_id, direction_from_parent_to_child) values (?,?,?)", "#{child_room[0]}", "#{parent_room[0]}", "#{direction}")
   end
 
+  def self.get_directions(location_id)
+    Database.execute("select direction_from_parent_to_child from exits where parent_room_id = ?", location_id)
+  end
+
+  def self.get_new_location_id(direction, player_location)
+    Database.execute("select child_room_id from exits where direction_from_parent_to_child = ? and parent_room_id = ?", direction, player_location)
+  end
+
 end
