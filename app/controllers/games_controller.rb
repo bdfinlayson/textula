f890b8dfa23da91722618f036825aeb88eadbf45
Bryan Finlayson <bdfinlayson@gmail.com>
2015-05-18 20:16:02 -0500

    remove thread sleep commands

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -12,22 +12,15 @@ class GamesController
   end
 
   def create
-    sleep 1.2
     puts "For starters, please enter your name.\n"
     @player_name = STDIN.gets.chomp
-    sleep 1.2
     puts "Nice to meet you, #{@player_name}!\n"
-    sleep 1.2
     puts "What's the name of your game?\n"
     @game_name = STDIN.gets.chomp
-    sleep 1.2
     puts "#{@game_name} sounds like an awesome game!\n"
-    sleep 1.2
     puts "Please add a description for #{@game_name}. For example, you could say 'adventure-fantasy'.\n"
     @game_description = STDIN.gets.chomp
-    sleep 1.2
     puts "Great! Please use the following menu options to further customize #{@game_name}!\n"
-    sleep 1.2
     add_new_game
     @game_id = get_game_id
   end
