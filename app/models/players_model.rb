e42751625e317fcf75db73945f4a38be340915b9
Bryan Finlayson <bdfinlayson@gmail.com>
2015-05-19 21:20:20 -0500

    add edit and delete tests for player model

--- app/models/players_model.rb
+++ app/models/players_model.rb
@@ -13,7 +13,19 @@ class PlayersModel
     Database.execute("select * from players")
   end
 
-  def self.find_player(game_id)
-    Database.execute("select * from players where game_id = ?", game_id)
+  def self.find_player(player_id)
+    Database.execute("select * from players where id = ?", player_id)
+  end
+
+  def self.delete_player(player_id)
+    Database.execute("delete from players where id = ?", player_id)
+  end
+
+  def self.edit_player_location(player_id, new_value)
+    Database.execute("update players set location = ? where id = ?", new_value, player_id)
+  end
+
+  def self.edit_player_name(player_id, new_value)
+    Database.execute("update players set player_name = ? where id = ?", new_value, player_id)
   end
 end
--- tests/models/test_players_model.rb
+++ tests/models/test_players_model.rb
@@ -36,8 +36,57 @@ describe '#get_all_players' do
       PlayersModel.create(5,6,"John")
     end
     it 'should return all info for the desired player' do
-      game_id = 4
-      assert_equal [4,4,5,"Jen"], PlayersModel.find_player(game_id).flatten
+      player_id = 4
+      assert_equal [4,4,5,"Jen"], PlayersModel.find_player(player_id).flatten
+    end
+  end
+
+  describe '#delete player' do
+    describe 'deleting a player' do
+      before do
+      PlayersModel.create(1,2,"Byran")
+      PlayersModel.create(2,3,"Adam")
+      PlayersModel.create(3,4,"Jill")
+      PlayersModel.create(4,5,"Jen")
+      PlayersModel.create(5,6,"John")
+      end
+      it 'should delete a player' do
+        PlayersModel.delete_player(4)
+        assert_empty PlayersModel.find_player(4)
+      end
+    end
+  end
+
+  describe '#edit_player_location' do
+    describe 'editing a player location' do
+      before do
+      PlayersModel.create(1,2,"Byran")
+      PlayersModel.create(2,3,"Adam")
+      PlayersModel.create(3,4,"Jill")
+      PlayersModel.create(4,5,"Jen")
+      PlayersModel.create(5,6,"John")
+      end
+      it 'should edit the player location' do
+        PlayersModel.edit_player_location(3, 1)
+        assert_equal [3,3,1,"Jill"], PlayersModel.find_player(3).flatten
+      end
+    end
+  end
+
+
+  describe '#edit_player_name' do
+    describe 'editing a player name' do
+      before do
+      PlayersModel.create(1,2,"Byran")
+      PlayersModel.create(2,3,"Adam")
+      PlayersModel.create(3,4,"Jill")
+      PlayersModel.create(4,5,"Jen")
+      PlayersModel.create(5,6,"John")
+      end
+      it 'should edit the player name' do
+        PlayersModel.edit_player_name(3, "James")
+        assert_equal [3,3,4,"James"], PlayersModel.find_player(3).flatten
+      end
     end
   end
 end
