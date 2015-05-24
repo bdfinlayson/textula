6209774fb2fd3b955637106bdda8ea961b314ffa
Bryan Finlayson <bdfinlayson@gmail.com>
2015-05-24 17:47:54 -0500

    add environment.current method

--- lib/environment.rb
+++ lib/environment.rb
@@ -1,5 +1,14 @@
 require 'active_record'
+Dir["./lib/*.rb"].each { |file| require file }
+Dir["./app/**/*.rb"].each { |file| require file }
 
 class Environment
 
+  def self.current
+    ENV["TEST"] ? "test" : "production"
+  end
 end
+
+
+connection_details = YAML::load(File.open('config/database.yml'))[Environment.current]
+ActiveRecord::Base.establish_connection(connection_details)
